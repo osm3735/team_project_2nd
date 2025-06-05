95115cc7d6bddfb7df1cf6098996647c678a20d9
osm3735 <osm3735@github.com>
2025-06-05 15:50:25 +0900

    py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/index", response_class=HTMLResponse)
+@app.get("/index.html", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/early_morning_coffee.html", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("early_morning_coffee.html", {"request": request})
+
+@app.get("/findstore.html", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("findstore.html", {"request": request})
